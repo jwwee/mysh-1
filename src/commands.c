3dc721c06a9ea8ffdcba2eb64e0d0120e1865bc3
jwwee <jwweenov@naver.com>
2017-11-14 20:13:03 +0900

    process creation and signal handling

--- src/commands.c
+++ src/commands.c
@@ -2,9 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-
+#include <unistd.h>
 #include "commands.h"
 #include "built_in.h"
+#include <sys/types.h>
+#include <wait.h>
+
 
 static struct built_in_command built_in_commands[] = {
   { "cd", do_cd, validate_cd_argv },
@@ -34,26 +37,42 @@ int evaluate_command(int n_commands, struct single_command (*commands)[512])
     struct single_command* com = (*commands);
 
     assert(com->argc != 0);
+	int built_in_pos = is_built_in_command(com->argv[0]);
+    int pid;
+	int status;
 
-    int built_in_pos = is_built_in_command(com->argv[0]);
-    if (built_in_pos != -1) {
-      if (built_in_commands[built_in_pos].command_validate(com->argc, com->argv)) {
+	if (built_in_pos != -1) {
+      if (built_in_commands[built_in_pos].command_validate(com->argc, com->argv)) 
+	  {
         if (built_in_commands[built_in_pos].command_do(com->argc, com->argv) != 0) {
           fprintf(stderr, "%s: Error occurs\n", com->argv[0]);
         }
-      } else {
+      }
+	  else 
+	  {
         fprintf(stderr, "%s: Invalid arguments\n", com->argv[0]);
         return -1;
       }
-    } else if (strcmp(com->argv[0], "") == 0) {
+    } 
+	else if (strcmp(com->argv[0], "") == 0) {
       return 0;
-    } else if (strcmp(com->argv[0], "exit") == 0) {
-      return 1;
-    } else {
-      fprintf(stderr, "%s: command not found\n", com->argv[0]);
-      return -1;
     }
-  }
+	else if (strcmp(com->argv[0], "exit") == 0) {
+      return 1;
+    } 
+	else {
+	  pid=fork();
+	  if(pid==0){
+	  	execv(com->argv[0],com->argv);
+	    fprintf(stderr, "%s: command not found\n", com->argv[0]);
+	  }
+	  else{
+	  wait(&status);
+   // fprintf(stderr, "%s: command not found\n", com->argv[0]);
+      return -1;
+	  }
+    }
+}
 
   return 0;
 }
--- src/signal_handlers.c
+++ src/signal_handlers.c
@@ -1,11 +1,19 @@
 #include "signal_handlers.h"
+#include <signal.h>
+#include <stdio.h>
 
 void catch_sigint(int signalNo)
 {
+  signal(SIGINT, catch_sigint);
+  printf("\nctrl+c ignored\n");
+  fflush(stdout);
   // TODO: File this!
 }
 
 void catch_sigtstp(int signalNo)
 {
+  signal(SIGTSTP, catch_sigtstp);
+  printf("\nctrl+z ignored\n");
+  fflush(stdout);
   // TODO: File this!
 }
